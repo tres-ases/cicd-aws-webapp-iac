2af935c003a3d4d0dd975ce24173f302ddd5320c
Angelo Morgado <amorgado@pentafinanciero.cl>
2020-11-25 21:45:58 -0300

    FIX: terraform state

--- main.tf
+++ main.tf
@@ -5,11 +5,12 @@ terraform {
     workspaces {
       name = "cosmos-demo"
     }
+    credentials "app.terraform.io" {
+      token = "AnysN9K0X2nyHQ.atlasv1.uzblW5Bt9C86AsB2rKuTzcSz1xWaj6qCV7sklNjfZ3fhvZRPQVJQiYhSYZJToN9FrVM"
+    }
   }
 }
-credentials "app.terraform.io" {
-  token = "AnysN9K0X2nyHQ.atlasv1.uzblW5Bt9C86AsB2rKuTzcSz1xWaj6qCV7sklNjfZ3fhvZRPQVJQiYhSYZJToN9FrVM"
-}
+
 provider "aws" {
   region = var.region
 }
