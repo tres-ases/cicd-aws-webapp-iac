4130fc4586f458e898b89ea0e85f7f3d800e6851
Angelo Morgado <amorgado@pentafinanciero.cl>
2020-11-25 21:43:40 -0300

    FIX: terraform state

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ terraform {
     }
   }
 }
+credentials "app.terraform.io" {
+  token = "AnysN9K0X2nyHQ.atlasv1.uzblW5Bt9C86AsB2rKuTzcSz1xWaj6qCV7sklNjfZ3fhvZRPQVJQiYhSYZJToN9FrVM"
+}
 provider "aws" {
   region = var.region
 }
